55da9e5d8d4df3f49e76a9c93891487bc034b055
neuro <neuro@well.com>
2013-11-20 23:00:20 +0000

    Theme import from weo front (4.0) plus include tweaks from leo build

--- web/current/index.php
+++ web/current/index.php
@@ -4,12 +4,20 @@
 ?>
 <?php
 include "assets/inc/00-functions.inc";
+$site_name = "browsers.evolt.org";
+$page_title = "browsers.evolt.org";
+$short_title = "Browser Archive";
+$page_type = "page";
 include "assets/inc/10-header.inc";
 include "assets/inc/20-message.inc";
 ?>
-
-<p class="notice"><strong>Notice</strong> - Your message! </p>
-
+<div class="wrapper">
+    <section id="content" class="full-width" role="main">
+    	<div class="p100">
+			<p class="notice error"><strong>Pardon our dust!</strong> This iteration of <strong>evolt.org</strong> is under alpha test, containing content which has not yet been finalized.</p>
+		</div>
+	</section>
+</div>
 <?
 include "assets/inc/99-footer.inc";
 ?>
